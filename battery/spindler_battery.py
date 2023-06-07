740130ba9855beb810ac3d2ba756c3043fbcc3fc
Sathwik Doddi <sathwik.doddi@gmail.com>
2023-06-07 11:44:34 -0400

    task two step 5

---
+++ battery/spindler_battery.py
@@ -0,0 +1,16 @@
+from battery import Battery
+
+class SpindlerBattery(Battery):
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+        self.years_to_service = 2
+
+    def needs_service(self):
+        temp_date = self.last_service_date
+        temp_date.replace(year=self.last_service_date + self.years_to_service)
+        date_which_battery_should_be_serviced_by = temp_date
+        if date_which_battery_should_be_serviced_by < self.current_date:
+            return True
+        else:
+            return False
